221bb13e15be49924dbe37437c41976c47bbad5d
JongOne <cowhddnjsdl0@naver.com>
2022-07-28 17:30:31 +0900

    css 완성
    페이지스 만들기 필요

--- src/App.js
+++ src/App.js
@@ -1,80 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
-import Header from "./header/Header";
-import Form from "./form/Form";
-import List from './list/List';
+import Layout from './layout/Layouy';
 
 function App() {
 
-  // inputs의 기본값을 줌
-  const [inputs, setInputs] = useState({
-    title: '',
-    text: ''
-  });
-    
-  // 제목과 내용을 inputs에 넣어줌
-  const { title, text } = inputs;
-
-  // 리스트를 만드는
-  const onCreate = () => {
-    const todo = {
-      id: nextId.current,
-      title,
-      text,
-      isDone : false
-    };
-    setTodos(todos.concat(todo));
- 
-    setInputs({
-      title: '',
-      text: ''
-    });
-    nextId.current += 1;
-  };
- 
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-
-
-  const [todos, setTodos] = useState([]);
-  //아이디에 기본값을 준다
-  const nextId = useRef(0);
   
-  
-  const onRemove = (id) => {
-    // todo.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = todo.id 가 id 인 것을 제거함
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
-  const onToggle = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-      )
-    );
-  };
   return (
-    <>
-      <div className="App">
-        <Header />
-        <Form
-          title={title}
-          text={text}
-          onChange={onChange}
-          onCreate={onCreate}
-        />
-        <List 
-          todos={todos} 
-          onRemove={onRemove} 
-          onToggle={onToggle}/>
-      </div>
-    </>
+    <Layout/>
   );
 }
 
--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -5,9 +5,9 @@ import "./style.css"
 function Todo({ todo, onRemove, onToggle }) {
     return (
             <div className='list_item'>
-                <div>{todo.title}</div> <span>{todo.text}</span>
-                <button onClick={() => onToggle(todo.id)}>완료하기</button>
-                <button onClick={() => onRemove(todo.id)}>삭제</button>
+                <div className='todohead'>제목 :{todo.title}</div> <div className='todobody'>할일 :{todo.text}</div>
+                <button className='btn1' onClick={() => onToggle(todo.id)}>완료하기</button>
+                <button className='btn1' onClick={() => onRemove(todo.id)}>삭제</button>
             </div>
     );
 }
